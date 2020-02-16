6ee480fc92c6f616b35c38f1ffe27df54f0a7b8d
юра гаравецкий <56477764+yngharavetskyy@users.noreply.github.com>
2020-02-16 14:54:31 +0200

    commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function openMobileNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+window.onload = function() {
+  var input = document.querySelector("#s-in-form");
+  var button = document.querySelector("#join-button");
+  if (input && button) {
+    button.addEventListener("click", e => {
+      if (input.value !== "") {
+        alert("Вы зарегистрировались!");
+      }
+    });
+  }
+};
